cc851c2ee324249fac3f9a55e017ee47386e884f
Niranjan <luitel.niranjan@gmail.com>
2025-05-07 17:29:03 +0545

    Day 1

---
+++ Calculator.py
@@ -0,0 +1,39 @@
+x = float(input("Enter the first number: "))
+y = float(input("Enter the second number:"))
+Operation = input("Enter the operation (+,-,/,*):")
+
+def add_num(num1, num2):
+    result = num1 + num2
+    x = round(result, 4)
+    #integer_number = int(result)
+    print("The sum of two numbers is:", x)
+
+def difference_num(num1, num2):
+    result = num1 - num2
+    print("The difference of two numbers is: ", result)
+
+def multiply_num(num1, num2):
+    result = num1 * num2 
+    print("The product of two numbers is: ", result)
+
+def remainder(num1, num2):
+    result = num1 / num2 
+    x = round(result, 4)
+    print("The remainder is: ", x)
+
+if Operation == '+':
+    add_num(x,y)
+
+elif Operation == '-':
+    difference_num(x,y)
+
+elif Operation == '*':
+    multiply_num(x,y) 
+
+elif Operation == "/":
+    
+    if y == 0:
+        print('Math Error')
+    else: 
+        remainder(x,y)
+   
---
+++ Revise.py
@@ -0,0 +1,22 @@
+x = float(input("Enter the first number: "))
+y = float(input("Enter the second number: "))
+
+def add_num(num1, num2):
+    result = num1 + num2
+    print(result)
+add_num(x, y)
+
+def difference_num(num1, num2):
+    result = num1 - num2
+    print(result)
+difference_num()
+
+def multiply_num(num1, num2):
+    result = num1 * num2 
+    print(result)
+multiply_num()
+
+def remainder(num1, num2):
+    result = num1 / num2 
+    print(result)
+remainder()
