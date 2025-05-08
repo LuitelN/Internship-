7bb62f8b773726669dac2052e4d03b2d9b0fe3aa
Niranjan <luitel.niranjan@gmail.com>
2025-05-08 10:14:25 +0545

    Creating Classes

--- Revise.py
+++ Revise.py
@@ -1,10 +1,20 @@
-x = 0
+class Student: 
+    def __init__(self, name, age, rollNum):
+        self.name = name 
+        self.age = age 
+        self.rollNum = rollNum
 
-while x < 10:
-    print("x:", x)
-    if x == 5:
-        print("Breaking...")
-        break
-    x += 1
+p1 = Student("John", 20, 1)
+p2 = Student("Harry", 22, 2)
+p3 = Student("Peter", 21, 3)
+print(p1.name)
+print(p1.age)
+print(p1.rollNum)
 
-print("End")
+print(p2.name)
+print(p2.age)
+print(p2.rollNum)
+
+print(p3.name)
+print(p3.age)
+print(p3.rollNum)
