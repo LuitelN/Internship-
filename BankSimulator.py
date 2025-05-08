532ab6fd9c732a45bb17d54864ed78611e9aa012
Niranjan <luitel.niranjan@gmail.com>
2025-05-08 12:51:07 +0545

    Created Classes for the users

---
+++ BankSimulator.py
@@ -0,0 +1,20 @@
+class User: 
+    def __init__(self, Name, Account_Number, Balance):
+        self.name = Name 
+        self.number = Account_Number
+        self.balance = Balance 
+
+u1 = User("A", 1 , 10000 )
+u2 = User("B", 2 , 20000 )
+u3 = User("C", 3 , 40000 )
+u4 = User("D", 4 , 50000 )
+u5 = User("E", 5 , 15000 )
+
+print("Account Holder:", u1.name, "\nAccount Number:", u1.number, "\nAccount Balance: ", u1.balance)
+print("Account Holder:", u2.name, "\nAccount Number:", u2.number, "\nAccount Balance: ", u2.balance)
+print("Account Holder:", u3.name, "\nAccount Number:", u3.number, "\nAccount Balance: ", u3.balance)
+print("Account Holder:", u4.name, "\nAccount Number:", u4.number, "\nAccount Balance: ", u4.balance)
+print("Account Holder:", u5.name, "\nAccount Number:", u5.number, "\nAccount Balance: ", u5.balance)
+
+
+        
