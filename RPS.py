88d46740436ad3f88e89988c7d64f138f14e9be7
Niranjan <luitel.niranjan@gmail.com>
2025-05-13 13:15:46 +0545

    Implemented loops but not working properly

--- RPS.py
+++ RPS.py
@@ -1,9 +1,26 @@
 #Rock, Paper, Scissors 
 
 import random 
-Options = ["Rock", "Paper", "Scissors"]
+Computer = ["Rock", "Paper", "Scissors"]
 
 User = input("Rock, Paper, Scissor?: ")
 print(User)
-print(random.choice(Options))
+print(random.choice(Computer))
 
+#Conditions 
+
+if User == "Rock" and Computer == "Scissor":
+    print("User Wins!")
+elif User == "Rock" and Computer == "Paper":
+    print("Computer Wins!")
+elif User == "Paper" and Computer == "Rock":
+    print("User Wins!")
+elif User == "Paper" and Computer == "Scissor":
+    print("Computer Wins!")
+if User == "Scissor" and Computer == "Paper":
+    print("User Wins!")
+elif User == "Scissor" and Computer == "Rock":
+    print("Computer Wins!")
+else: 
+    print("Draw")
+    
