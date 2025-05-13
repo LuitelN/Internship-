f7871cb60124fecd11f92bbdfca0a35f6ece0d8b
Niranjan <luitel.niranjan@gmail.com>
2025-05-13 14:04:41 +0545

    Completed the game

--- RPS.py
+++ RPS.py
@@ -1,26 +1,30 @@
 #Rock, Paper, Scissors 
 
 import random 
-Computer = ["Rock", "Paper", "Scissors"]
+computer = ["Rock", "Paper", "Scissors"]
 
-User = input("Rock, Paper, Scissor?: ")
-print(User)
-print(random.choice(Computer))
+user = input("Rock, Paper, Scissors?\n").strip()
+print("User chose: ", user)
 
 #Conditions 
+if user not in computer: 
+    print("Invalid input. Please choose : Rock, Paper or Scissors")
+else:
+    computer_choice = random.choice(computer)
+    print("Computer chose: ", computer_choice)
 
-if User == "Rock" and Computer == "Scissor":
-    print("User Wins!")
-elif User == "Rock" and Computer == "Paper":
-    print("Computer Wins!")
-elif User == "Paper" and Computer == "Rock":
-    print("User Wins!")
-elif User == "Paper" and Computer == "Scissor":
-    print("Computer Wins!")
-if User == "Scissor" and Computer == "Paper":
-    print("User Wins!")
-elif User == "Scissor" and Computer == "Rock":
-    print("Computer Wins!")
-else: 
-    print("Draw")
+    if user == "Rock" and computer_choice == "Scissors":
+        print("Result: User Wins!")
+    elif user == "Rock" and computer_choice == "Paper":
+        print("Result: Computer Wins!")
+    elif user == "Paper" and computer_choice == "Rock":
+        print("Result: User Wins!")
+    elif user == "Paper" and computer_choice == "Scissors":
+        print("Result: Computer Wins!")
+    elif user == "Scissors" and computer_choice == "Paper":
+        print("Result: User Wins!")
+    elif user == "Scissors" and computer_choice == "Rock":
+        print("Result: Computer Wins!")
+    else: 
+        print("Result: Draw")
     
