ae0939d4d411d7ac4ee9b8094d20f71e18cc5159
Niranjan <luitel.niranjan@gmail.com>
2025-05-14 15:04:37 +0545

    Test case

--- Trial.py
+++ Trial.py
@@ -1,32 +1,4 @@
-# # A simple decorator function
-# def decorator(func):
-  
-#     def wrapper():
-#         print("Before calling the function.")
-#         func()
-#         print("After calling the function.")
-#         #greet()
-#     return wrapper
-
-# # Applying the decorator to a function
-# @decorator
-
-# def greet():
-#     print("Hello, World!")
-
-# greet()
-
-n = int(input().strip())
-if n%2 != 0:
-    print("Weird")
-
-elif n%2 == 0 & 2<= n <= 5:
-    print("Not Weird")
-
-elif n%2 ==0 & 2<= n <= 5:
-    print("Weird")
-
-else: 
-    n%2 == 0 & n > 20
-    print("Not Weird")
+x  = 1 
+y = 2 
+print(x+y)
 
